c0321b6b21aee38bc38c6cc82599576ede560528
Hary Pham <haryphamdev@gmail.com>
2021-04-20 20:48:45 +0700

    update

--- src/config/config.js
+++ src/config/config.js
@@ -1,26 +1,25 @@
 require('dotenv').config();
 
-module.exports =
-{
-  "development": {
-    "username": 'root',
-    "password": null,
-    "database": 'nodejwt',
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+module.exports = {
+  development: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: '127.0.0.1',
+    dialect: 'mysql'
   },
-  "test": {
-    "username": process.env.DB_USERNAME,
-    "password": process.env.DB_PASSWORD,
-    "database": process.env.DB_NAME,
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+  test: {
+    username: 'database_test',
+    password: null,
+    database: 'database_test',
+    host: '127.0.0.1',
+    dialect: 'mysql'
   },
-  "production": {
-    "username": process.env.DB_USERNAME,
-    "password": process.env.DB_PASSWORD,
-    "database": process.env.DB_NAME,
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+  production: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOSTNAME,
+    dialect: 'mysql'
   }
-}
+};
